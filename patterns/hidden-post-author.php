f03c231af98f484756aa949ce89b09a085797ebb
Lax Mariappan <elaxmariappan@gmail.com>
2024-06-15 23:06:21 +0530

    add author avatar

--- patterns/hidden-post-author.php
+++ patterns/hidden-post-author.php
@@ -5,6 +5,10 @@
  * Inserter: no
  */
 ?>
-<div class="wp-block-group">
-    <!-- wp:post-author {"avatarSize":96,"showBio":true,"isLink":true} /-->
-</div>
+
+    <div class="wp-block-group">
+        <!-- wp:post-author {"avatarSize":96,"showBio":true,"isLink":true,"linkTarget":"_blank"} /-->
+    </div>
+<!-- wp:separator {"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|40"}}},"backgroundColor":"contrast-3","className":"is-style-wide"} -->
+<hr class="wp-block-separator has-text-color has-contrast-3-color has-alpha-channel-opacity has-contrast-3-background-color has-background is-style-wide" style="margin-bottom:var(--wp--preset--spacing--40)"/>
+<!-- /wp:separator -->
