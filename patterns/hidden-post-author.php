9b2bdc70a27498d3b53e2d25df874467128eedca
Lax Mariappan <elaxmariappan@gmail.com>
2024-06-15 22:38:59 +0530

    add author bio

---
+++ patterns/hidden-post-author.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Title: Post Author
+ * Slug: salvatore/hidden-post-author
+ * Inserter: no
+ */
+?>
+<div class="wp-block-group">
+    <!-- wp:post-author {"avatarSize":96,"showBio":true,"isLink":true} /-->
+</div>
