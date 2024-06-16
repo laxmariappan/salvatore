2fb4697fbb16f04eb4dca8af4d6822a6d3f7132c
Lax Mariappan <elaxmariappan@gmail.com>
2024-06-16 12:49:22 +0530

    update patterns and dark mode toggle

--- patterns/hidden-post-author.php
+++ patterns/hidden-post-author.php
@@ -7,8 +7,5 @@
 ?>
 
     <div class="wp-block-group">
-        <!-- wp:post-author {"avatarSize":96,"showBio":true,"isLink":true,"linkTarget":"_blank"} /-->
+        <!-- wp:post-author {"avatarSize":96,"showBio":true,"isLink":true,"style":{"spacing":{"padding":{"top":"var:preset|spacing|10","bottom":"var:preset|spacing|10","left":"var:preset|spacing|10","right":"var:preset|spacing|10"}}},"backgroundColor":"accent","fontSize":"medium"} /-->
     </div>
-<!-- wp:separator {"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|40"}}},"backgroundColor":"contrast-3","className":"is-style-wide"} -->
-<hr class="wp-block-separator has-text-color has-contrast-3-color has-alpha-channel-opacity has-contrast-3-background-color has-background is-style-wide" style="margin-bottom:var(--wp--preset--spacing--40)"/>
-<!-- /wp:separator -->
---
+++ patterns/hidden-post-meta.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Title: Post meta
+ * Slug: salvatore/hidden-post-meta
+ * Inserter: no
+ */
+?>
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group">
+	<!-- wp:group {"style":{"spacing":{"blockGap":"0.3em"}},"layout":{"type":"flex","justifyContent":"left"}} -->
+	<div class="wp-block-group">
+        <?php
+        // Check if the post has been updated.
+        if ( get_the_modified_time( 'U' ) !== get_the_time( 'U' ) ) : ?>
+            <!-- wp:paragraph {"fontSize":"small","textColor":"contrast-2"} -->
+            <p class="has-small-font-size has-contrast-2-color has-text-color">
+                <strong><?php echo esc_html_x( 'Last Updated on', 'Prefix for the post updated on: Last Updated on', 'salvatore' ); ?></strong>
+            </p>
+            <!-- /wp:paragraph -->
+            <!-- wp:post-date {"format":"M j, Y","isLink":true, "displayType":"modified" } /-->
+        <?php else : ?>
+            <!-- wp:paragraph {"fontSize":"small","textColor":"contrast-2"} -->
+            <p class="has-small-font-size has-contrast-2-color has-text-color">
+                <strong><?php echo esc_html_x( 'Published on', 'Prefix for the post published on: Published on', 'salvatore' ); ?></strong>
+            </p>
+            <!-- /wp:paragraph -->
+            <!-- wp:post-date {"format":"M j, Y","isLink":true } /-->
+        <?php endif; ?>
+
+		<!-- wp:paragraph {"textColor":"contrast-2"} -->
+		<p class="has-contrast-2-color has-text-color">—</p>
+		<!-- /wp:paragraph -->
+
+		<!-- wp:paragraph {"fontSize":"small","textColor":"contrast-2"} -->
+		<p class="has-small-font-size has-contrast-2-color has-text-color"><?php echo esc_html_x( 'by', 'Prefix for the post author block: By author name', 'twentytwentyfour' ); ?></p>
+		<!-- /wp:paragraph -->
+
+		<!-- wp:post-author-name {"isLink":true} /-->
+
+		<!-- wp:post-terms {"term":"category","prefix":"<?php echo esc_html_x( 'in ', 'Prefix for the post category block: in category name', 'twentytwentyfour' ); ?>"} /-->
+
+	</div>
+	<!-- /wp:group -->
+</div>
+<!-- /wp:group -->
